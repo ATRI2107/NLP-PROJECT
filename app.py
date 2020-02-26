126550e928c30865233025d8267b388ed37c325e
ATRI2107 <atrichaturvedi@gmail.com>
2020-02-26 22:37:33 +0530

    Essay post route configured

--- app.py
+++ app.py
@@ -5,5 +5,10 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return render_template('index.html')
+@app.route('/predict',methods=['GET','POST'])
+def predict():
+    if request.method=='POST':
+        data=request.form['essay']
+        return render_template('predict.html',data=data)
 if __name__=='__main__':
 	app.run(debug=True)
